7cfd8e6e7a45519d0a4257d23d70b04d4152f929
k-jdev <svv.ooj@gmail.com>
2025-09-29 12:51:43 +0300

    feat(browse): add the browse page header

---
+++ src/features/browse/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="text-center mt-10">
+      <h1 className="text-[44px] text-black font-semibold">
+        Explore Your Next Skill
+      </h1>
+      <p className="text-slate-600  text-lg">
+        Discover a world of knowledge and find the perfect skill to learn or
+        share.
+      </p>
+    </header>
+  );
+}
+
+export default Header;
