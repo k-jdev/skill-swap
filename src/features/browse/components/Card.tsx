3889e42d2ea57bc28305ea91fd56b83ddd8bbfe5
k-jdev <svv.ooj@gmail.com>
2026-03-16 02:06:13 +0200

    update: filter and search skills

--- src/features/browse/components/Card.tsx
+++ src/features/browse/components/Card.tsx
@@ -22,7 +22,7 @@ function Card({ skill }: CardProps) {
           alt={skill.skill_title}
           width={300}
           height={200}
-          className="w-full h-48 rounded-t-2xl object-contain"
+          className="w-full h-48 rounded-t-2xl object-cover"
         />
         <div className="absolute bottom-0 left-0 right-0 h-16 bg-gradient-to-t from-black/50 to-transparent rounded-t-2xl"></div>
       </div>
