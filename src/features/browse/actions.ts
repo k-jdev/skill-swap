f87a3e9fe53a433b672eda35f1fc63028d4f816b
k-jdev <svv.ooj@gmail.com>
2026-03-16 02:06:13 +0200

    feat(browse): add skill search and filtering

--- src/features/browse/actions.ts
+++ src/features/browse/actions.ts
@@ -2,13 +2,20 @@
 
 import { createClient } from "@/shared/utils/supabase/server";
 
-export async function getSkillAction() {
+export async function getSkillAction(category?: string, searchTerm?: string) {
   const supabase = await createClient();
 
-  const { data, error } = await supabase
-    .from("skills")
-    .select("*")
-    .order("created_at", { ascending: false });
+  let query = supabase.from("skills").select("*");
+
+  if (category && category != "All") {
+    query = query.eq("category", category);
+  }
+  if (searchTerm) {
+    query = query.ilike("skill_title", `%${searchTerm}%`);
+  }
+  const { data, error } = await query.order("created_at", {
+    ascending: false,
+  });
 
   if (error) return { error: error.message };
   return data;
--- src/features/browse/components/CardGrid.tsx
+++ src/features/browse/components/CardGrid.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import { Card } from "@/features/browse/components";
 import { getSkillAction } from "../actions";
+import useSkillsStore from "@/shared/store/useSkillsStore";
 
-async function CardGrid() {
-  const skills = await getSkillAction();
-  console.log("skills:", skills);
-  if (!skills || "error" in skills) return null;
+function CardGrid() {
+  const { skillTitle, skillCategory }: any = useSkillsStore();
+  const [skills, setSkills] = useState<any[]>([]);
+
+  useEffect(() => {
+    getSkillAction(skillCategory, skillTitle).then((result) => {
+      if (result && !("error" in result)) {
+        setSkills(result);
+      }
+    });
+  }, [skillTitle, skillCategory]);
+
+  if (!skills.length) return null;
   return (
     <div className="mt-5 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
       {skills.map((skill) => (
