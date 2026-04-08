86565698617f0502eee3eeab729b13aa4899e062
k-jdev <svv.ooj@gmail.com>
2026-04-09 02:41:07 +0300

    feat(skill): flesh out the skill detail page

--- src/features/skill/components/detail/index.tsx
+++ src/features/skill/components/detail/index.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import SkillDetailHeader from "./SkillDetailHeader";
+import SkillDetailAbout from "./SkillDetailAbout";
+import SkillDetailReviews from "./SkillDetailReviews";
 function SkillDetail({ skillId }: { skillId: string }) {
   return (
     <section>
       <SkillDetailHeader skillId={skillId} />
+      <div className="flex gap-8 ">
+        <div className="grid gap-8 ">
+          <SkillDetailAbout />
+          <SkillDetailReviews />
+        </div>
+      </div>
     </section>
   );
 }
