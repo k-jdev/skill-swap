a633a13bd1dc795ffc535fd23b89cd76800461d7
k-jdev <svv.ooj@gmail.com>
2026-04-10 01:56:03 +0300

    update: completed ui of skill detail

--- src/features/skill/components/detail/index.tsx
+++ src/features/skill/components/detail/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import SkillDetailHeader from "./SkillDetailHeader";
 import SkillDetailAbout from "./SkillDetailAbout";
 import SkillDetailReviews from "./SkillDetailReviews";
+import SkillDetailProfile from "./SkillDetailProfile";
+import SkillDetailBuy from "./SkillDetailBuy";
 function SkillDetail({ skillId }: { skillId: string }) {
   return (
     <section>
@@ -10,6 +12,12 @@ function SkillDetail({ skillId }: { skillId: string }) {
         <div className="grid gap-8 ">
           <SkillDetailAbout />
           <SkillDetailReviews />
+        </div>
+        <div className="flex flex-col w-[360px] shrink-0">
+          <div className="mb-6">
+            <SkillDetailBuy />
+          </div>
+          <SkillDetailProfile />
         </div>
       </div>
     </section>
