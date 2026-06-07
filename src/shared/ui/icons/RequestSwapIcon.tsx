b188838263538f54aafb7e775463821b1b1f8384
k-jdev <svv.ooj@gmail.com>
2026-06-07 17:35:17 +0300

    fix: ui and changed how  it works section

---
+++ src/shared/ui/icons/RequestSwapIcon.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export function RequestSwapIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M16 3h5v5" />
+      <path d="M8 3H3v5" />
+      <path d="M21 3l-7 7" />
+      <path d="M3 21l7-7" />
+      <path d="M3 3l18 18" />
+    </svg>
+  );
+}
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,3 +2,4 @@ import Button from "./Button";
 import Input from "./Input";
 import Modal from "./Modal";
 export { Button, Input, Modal };
+export * from "./icons";
